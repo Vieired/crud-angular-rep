40c2b6babcd5b90f8ba210d68d45084c3f6b56e1
egomesmtt <egomes@metatron.com.br>
2019-06-13 17:39:46 -0300

    Inserindo componente de tabela na tela index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ProdutosListComponent } from './produtos-list/produtos-list.component'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
