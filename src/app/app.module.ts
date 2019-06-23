ce483a513410d538cb2f5cd74ef70ffb12c4257b
Ed Gomes <logominus@gmail.com>
2019-06-23 20:39:45 -0300

    Criação dos componentes para as telas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProdutosListComponent } from './produtos-list/produtos-list.component'
+import { ProdutosListComponent } from './produtos-list/produtos-list.component';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component'
 
 @NgModule({
   declarations: [
     AppComponent, 
-    ProdutosListComponent
+    ProdutosListComponent, HomeComponent, ContatoComponent
   ],
   imports: [
     BrowserModule,
